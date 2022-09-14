1f258a13ad2e476ec9d1713297b0be4bd16a7789
Martin Pakandl <nzdm.martin@seznam.cz>
2022-09-14 08:31:18 +0200

    Prepared for pushing

---
+++ src/app/recipe-book/recipe-list/recipe-item/recipe-item.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-recipe-item',
+  templateUrl: './recipe-item.component.html',
+  styleUrls: ['./recipe-item.component.css']
+})
+export class RecipeItemComponent implements OnInit {
+@Input() recipe: any;
+@Output() recipeSelected = new EventEmitter<void>()
+
+constructor() {
+
+}
+  ngOnInit(): void {
+  }
+
+  onSelected() {
+    this.recipeSelected.emit();
+  }
+
+}
